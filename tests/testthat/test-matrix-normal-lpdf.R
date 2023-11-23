e61efe7bd19cbe241d75061090bdd7fc8503d51e
Josh Cowley <j.cowley1@ncl.ac.uk>
2023-11-23 12:31:33 +0000

    Fixed test
    R uses upper tri cholesky decompostion

--- tests/testthat/test-matrix-normal-lpdf.R
+++ tests/testthat/test-matrix-normal-lpdf.R
@@ -8,11 +8,12 @@ test_that("Matrix normal (log) density consistent", {
 
   A = matrix(rnorm(20), 10, 2)
   M = matrix(0, 10, 2)
-  U = diag(0.1, nrow = 10)
-  V = diag(0.8, nrow = 2)
+  U = 0.2 + diag(0.5, nrow = 10)
+  L_U = t(chol(U))
+  V = 0.8 + diag(0.2, nrow = 2)
 
-  out_stan <- matrix_normal_lpdf(A, M, U, V)
-  out_r <- matrixNormal::dmatnorm(A, M, U, V)
+  out_stan <- matrix_normal_halfcholesky_lpdf(A, M, L_U, V)
+  out_r <- matrixNormal::dmatnorm(A, M, U, V, log = TRUE)
 
   testthat::expect_equal(out_stan, out_r)
 })
